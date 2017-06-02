b97aa82e5f09bb1c578d7bda72f3e7d0ba4754e1
Sara Daqiq <saradaqiq@Saras-MacBook-Air.local>
2017-06-02 12:41:48 -0700

    s

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee{
+  constructor() {
+  	super();
+  	this.age = 10;
+  	this.job = 'find pollen';
+  	super.color = 'yellow';
+  	this.canFly = true;
+  	this.treasureChest = [];
+  }
+  forage() {
+  	this.treasureChest.push('treasure');
+  }
 };
